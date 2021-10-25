daa8c5716278195fb422d16018980bfeb71cf636
Luander <69652684+LuanderFarias@users.noreply.github.com>
2021-10-25 16:51:14 -0300

    Update screen.js

--- js/screen.js
+++ js/screen.js
@@ -1,37 +1,45 @@
 function main() {
-  const video = document.getElementById("video");
-  const start = document.getElementById("start");
-  const stop = document.getElementById("stop");
+	const video = document.getElementById("video");
+	const start = document.getElementById("start");
+	const stop = document.getElementById("stop");
 
-  var displayMediaOptions = {
-    video: {
-      cursor: "always",
-    },
-    audio: false,
-  };
+	var displayMediaOptions = {
+		video: {
+			cursor: "always",
+		},
+		audio: false,
+	};
 
-  start.onclick = function (e) {
-    startSharing();
-  };
-  stop.onclick = function (e) {
-    stopSharing();
-  };
+	start.onclick = function (e) {
+		startSharing();
+	};
 
-  async function startSharing() {
-    try {
-      video.srcObject = await navigator.mediaDevices.getDisplayMedia(
-        displayMediaOptions
-      );
-      } catch (error) {
-        console.log(error);
-      }
-    }
+	stop.onclick = function (e) {
+		stopSharing();
+	};
 
-    function stopSharing() {
-      let tracks = video.srcObject.getTracks();
-      tracks.forEach((track) => track.stop());
-      video.srcObject = null;
-    }
+	async function startSharing() {
+		let capture = null;
+		try {
+			capture = await navigator.mediaDevices.getDisplayMedia(
+				displayMediaOptions
+			);
+
+			capture
+				.getVideoTracks()[0]
+				.addEventListener("ended", () => stopSharing());
+
+			video.srcObject = capture;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	function stopSharing() {
+		let tracks = video.srcObject.getTracks();
+		tracks.forEach((track) => track.stop());
+		video.srcObject = null;
+	}
 }
 
 main();
